b6e5cd169425480fc335d8a4e097ea32e11d5fd0
xuxy <xuxiaoyu2010@gmail.com>
2021-01-12 15:05:14 +0800

    分包

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,19 @@
 extern crate serde_derive;
 extern crate toml;
 #[macro_use]
-extern crate log; 
+extern crate log;
 
-use reqwest::Method;
-use serde::Deserialize;
-use quick_xml::de::{from_str, DeError};
+
+mod webdav;
+
+
 use std::fs;
-use chrono::prelude::*;
+use webdav::{Webdav, Native, Account};
+
 
 #[derive(Deserialize, Debug)]
 struct Conf {
     webdav: Account,
-}
-
-#[derive(Deserialize, Debug)]
-struct Account {
-    username: String,
-    password: String,
 }
 
 fn init_config() -> Conf {
@@ -29,127 +25,6 @@ fn init_config() -> Conf {
     config
 }
 
-struct Webdav {
-    path: String,
-    account: Account,
-}
-
-impl Webdav {
-    fn new(path: &str, account: Account) -> Self {
-        Webdav {
-            path: path.to_string(),
-            account: account,
-        }
-    }
-    fn list(&self) -> Box<Vec<Filestatus>> {
-        let url = &self.path;
-        let client = reqwest::blocking::Client::new();
-        let body = client
-            .request(Method::from_bytes(b"PROPFIND").unwrap(), url)
-            .basic_auth(&self.account.username, Some(&self.account.password))
-            .send()
-            .unwrap()
-            .text()
-            .unwrap();
-        debug!("{}", body);
-        let multistatus: Multistatus = from_str(&body).unwrap();
-        let mut files: Vec<Filestatus> = Vec::new();
-
-        for response in multistatus.response {
-
-            let modify_time = DateTime::parse_from_rfc2822(&response.propstat.prop.getlastmodified).unwrap();
-            let f = Filestatus{
-                path: response.href,
-                lastmodified: modify_time.with_timezone(&Local),
-                contentlength: response.propstat.prop.getcontentlength,                
-                owner: response.propstat.prop.owner,
-                contenttype:response.propstat.prop.getcontenttype,
-                name:response.propstat.prop.displayname,
-            };
-            files.push(f)                
-
-        }
-        Box::new(files)
-    }
-}
-
-#[derive(Debug)]
-struct Filestatus {
-    path: String,
-    lastmodified: DateTime<Local>,
-    contentlength: u64,
-    owner: String,
-    contenttype: String,
-    name: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Multistatus {
-    response: Vec<Response>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Response {
-    href: String,
-    propstat: Propstat,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Propstat {
-    prop: Prop,
-    status: String
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Prop{
-    getlastmodified: String,
-    getcontentlength: u64,
-    owner: String,
-    getcontenttype: String,
-    displayname: String,
-}
-
-struct Native {
-    path: String
-}
-
-impl Native {
-    fn list(self) -> Box<Vec<Filestatus>> {
-        let mut files: Vec<Filestatus> = Vec::new();
-        match fs::read_dir(self.path) {
-            Ok(dir) => {                
-                for entry in dir {
-                    if let Ok(entry) = entry {
-                        // Here, `entry` is a `DirEntry`.
-                        if let Ok(metadata) = entry.metadata() {
-                            // Now let's show our entry's permissions!
-                            let f = Filestatus{
-                                path: entry.path().to_str().unwrap().to_string(),
-                                lastmodified: DateTime::from(metadata.modified().unwrap()),
-                                contentlength: metadata.len(),
-                                owner: "".to_string(),
-                                contenttype: "".to_string(),
-                                name: entry.file_name().into_string().unwrap(),
-                            };
-
-                            files.push(f);
-                            
-                        } else {
-                            println!("Couldn't get metadata for {:?}", entry.path());
-                        }
-                    }
-
-                }
-            },
-            Err(e)  => println!("{}",e),
-        }
-
-        Box::new(files)
-    }
-}
-
-
-
 fn main() {
    let config = init_config();
 
@@ -159,7 +34,7 @@ fn main() {
     println!("{:?}", res);
 
     println!("======================= local ===========================");
-    let native = Native{path:"schedule".to_string()};
+    let native = Native::new("schedule");
     let locals = native.list();
     println!("{:?}", locals);
 }
---
+++ src/webdav.rs
@@ -0,0 +1,137 @@
+use reqwest::Method;
+use quick_xml::de::{from_str};
+use std::fs;
+use chrono::prelude::*;
+
+
+#[derive(Debug)]
+pub struct Filestatus {
+    path: String,
+    lastmodified: DateTime<Local>,
+    contentlength: u64,
+    owner: String,
+    contenttype: String,
+    name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Multistatus {
+    response: Vec<Response>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    href: String,
+    propstat: Propstat,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Propstat {
+    prop: Prop,
+    status: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Prop{
+    getlastmodified: String,
+    getcontentlength: u64,
+    owner: String,
+    getcontenttype: String,
+    displayname: String,
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Account {
+    username: String,
+    password: String,
+}
+
+pub struct Webdav {
+    path: String,
+    account: Account,
+}
+
+impl Webdav {
+    pub fn new(path: &str, account: Account) -> Self {
+        Webdav {
+            path: path.to_string(),
+            account: account,
+        }
+    }
+    pub fn list(&self) -> Box<Vec<Filestatus>> {
+        let url = &self.path;
+        let client = reqwest::blocking::Client::new();
+        let body = client
+            .request(Method::from_bytes(b"PROPFIND").unwrap(), url)
+            .basic_auth(&self.account.username, Some(&self.account.password))
+            .send()
+            .unwrap()
+            .text()
+            .unwrap();
+        debug!("{}", body);
+        let multistatus: Multistatus = from_str(&body).unwrap();
+        let mut files: Vec<Filestatus> = Vec::new();
+
+        for response in multistatus.response {
+
+            let modify_time = DateTime::parse_from_rfc2822(&response.propstat.prop.getlastmodified).unwrap();
+            let f = Filestatus{
+                path: response.href,
+                lastmodified: modify_time.with_timezone(&Local),
+                contentlength: response.propstat.prop.getcontentlength,                
+                owner: response.propstat.prop.owner,
+                contenttype:response.propstat.prop.getcontenttype,
+                name:response.propstat.prop.displayname,
+            };
+            files.push(f)                
+
+        }
+        Box::new(files)
+    }
+}
+
+
+pub struct Native {
+    path: String
+}
+
+impl Native {
+    pub fn new(path: &str) -> Self {
+        Native{
+            path: path.to_string()
+        }
+    }    
+    pub fn list(self) -> Box<Vec<Filestatus>> {
+        let mut files: Vec<Filestatus> = Vec::new();
+        match fs::read_dir(self.path) {
+            Ok(dir) => {                
+                for entry in dir {
+                    if let Ok(entry) = entry {
+                        // Here, `entry` is a `DirEntry`.
+                        if let Ok(metadata) = entry.metadata() {
+                            // Now let's show our entry's permissions!
+                            let f = Filestatus{
+                                path: entry.path().to_str().unwrap().to_string(),
+                                lastmodified: DateTime::from(metadata.modified().unwrap()),
+                                contentlength: metadata.len(),
+                                owner: "".to_string(),
+                                contenttype: "".to_string(),
+                                name: entry.file_name().into_string().unwrap(),
+                            };
+
+                            files.push(f);
+                            
+                        } else {
+                            println!("Couldn't get metadata for {:?}", entry.path());
+                        }
+                    }
+
+                }
+            },
+            Err(e)  => println!("{}",e),
+        }
+
+        Box::new(files)
+    }
+}
